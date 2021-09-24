72c89b5b36026d0f05e1b5fed6f3adcc044dc61e
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-09-24 21:56:32 +0000

    Interface almost done

--- lib/screens/task.dart
+++ lib/screens/task.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
+
 class TaskScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(Icons.list, size: 30, color: Colors.lightBlue),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Tasky",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "15 Tasks",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
